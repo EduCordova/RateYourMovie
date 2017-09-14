c44a4895f4847b5324113991430a0cde2da0679e
luishmr0 <luchiitoz@gmail.com>
2017-09-13 22:21:08 -0500

    minor fixes

--- src/components/FooterCine.vue
+++ src/components/FooterCine.vue
@@ -4,10 +4,10 @@
         <div class="container">
           <div class="content has-text-centered">
             <p>
-              2017 © <strong>Rate your movie!</strong> by <a href="http://github.com/educordova">edu</a> and <a href="http://github.com">vader</a>.
+              2017 © <strong>Rate your movie!</strong> by <a href="http://github.com/educordova">edu</a> and <a href="http://github.com/luishmr0">vader</a>.
             </p>
             <p>
-              <a class="icon" href="https://github.com/luishmr0">
+              <a class="icon" href="https://github.com">
                 <i class="fa fa-github"></i> </a>
             </p>
           </div>
